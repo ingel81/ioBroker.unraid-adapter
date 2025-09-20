0d46300336462edeb056e988c8b9457a9f750979
ingel81 <ingel81@sgeht.net>
2025-09-20 22:13:56 +0200

    .

--- README.md
+++ README.md
@@ -33,7 +33,7 @@ You are almost done, only a few steps left:
     ```
 1. Add a new secret under https://github.com/ingel81/ioBroker.unraid-adapter/settings/secrets. It must be named `AUTO_MERGE_TOKEN` and contain a personal access token with push access to the repository, e.g. yours. You can create a new token under https://github.com/settings/tokens.
 
-1. Head over to [main.js](main.js) and start programming!
+1. Head over to [src/main.ts](src/main.ts) and start programming!
 
 ### Best Practices
 We've collected some [best practices](https://github.com/ioBroker/ioBroker.repositories#development-and-coding-best-practices) regarding ioBroker development and coding in general. If you're new to ioBroker or Node.js, you should
@@ -43,7 +43,8 @@ check them out. If you're already experienced, you should also take a look at th
 Several npm scripts are predefined for your convenience. You can run them using `npm run <scriptname>`
 | Script name | Description |
 |-------------|-------------|
-| `build` | Compile the React sources. |
+| `build` | Compile the TypeScript sources and the React admin UI. |
+| `build:ts` | Compile the TypeScript sources only. |
 | `watch` | Compile the React sources and watch for changes. |
 | `test:js` | Executes the tests you defined in `*.test.js` files. |
 | `test:package` | Ensures your `package.json` and `io-package.json` are valid. |
---
+++ src/main.ts
@@ -0,0 +1,88 @@
+import { Adapter, AdapterOptions } from '@iobroker/adapter-core';
+
+class UnraidAdapter extends Adapter {
+    public constructor(options: Partial<AdapterOptions> = {}) {
+        super({
+            ...options,
+            name: 'unraid-adapter',
+        });
+
+        this.on('ready', this.onReady.bind(this));
+        this.on('stateChange', this.onStateChange.bind(this));
+        this.on('unload', this.onUnload.bind(this));
+        // this.on("objectChange", this.onObjectChange.bind(this));
+        // this.on("message", this.onMessage.bind(this));
+    }
+
+    private async onReady(): Promise<void> {
+        this.log.info(`config baseUrl: ${this.config.baseUrl || '<not set>'}`);
+        this.log.info(
+            `config apiToken: ${this.config.apiToken ? `set (${this.config.apiToken.length} characters)` : '<not set>'}`
+        );
+
+        await this.setObjectNotExistsAsync('testVariable', {
+            type: 'state',
+            common: {
+                name: 'testVariable',
+                type: 'boolean',
+                role: 'indicator',
+                read: true,
+                write: true,
+            },
+            native: {},
+        });
+
+        this.subscribeStates('testVariable');
+
+        await this.setStateAsync('testVariable', true);
+        await this.setStateAsync('testVariable', { val: true, ack: true });
+        await this.setStateAsync('testVariable', { val: true, ack: true, expire: 30 });
+
+        const isPasswordValid = await this.checkPasswordAsync('admin', 'iobroker');
+        this.log.info(`check user admin pw iobroker: ${isPasswordValid}`);
+
+        const isInGroup = await this.checkGroupAsync('admin', 'admin');
+        this.log.info(`check group user admin group admin: ${isInGroup}`);
+    }
+
+    private onUnload(callback: () => void): void {
+        try {
+            callback();
+        } catch (e) {
+            callback();
+        }
+    }
+
+    private onStateChange(id: string, state: ioBroker.State | null | undefined): void {
+        if (state) {
+            this.log.info(`state ${id} changed: ${state.val} (ack = ${state.ack})`);
+        } else {
+            this.log.info(`state ${id} deleted`);
+        }
+    }
+
+    // private onObjectChange(id: string, obj: ioBroker.Object | null | undefined): void {
+    //     if (obj) {
+    //         this.log.info(`object ${id} changed: ${JSON.stringify(obj)}`);
+    //     } else {
+    //         this.log.info(`object ${id} deleted`);
+    //     }
+    // }
+
+    // private onMessage(obj: ioBroker.Message): void {
+    //     if (typeof obj === 'object' && obj.message) {
+    //         if (obj.command === 'send') {
+    //             this.log.info('send command');
+    //             if (obj.callback) {
+    //                 this.sendTo(obj.from, obj.command, 'Message received', obj.callback);
+    //             }
+    //         }
+    //     }
+    // }
+}
+
+if (module.parent) {
+    module.exports = (options: Partial<AdapterOptions> | undefined) => new UnraidAdapter(options);
+} else {
+    (() => new UnraidAdapter())();
+}
